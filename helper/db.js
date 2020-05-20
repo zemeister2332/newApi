c418e4403243d26b20c07256763ca408b07db0c0
zemeister <zemeister@localhost.localdomain>
2020-05-20 21:41:56 +0000

    mongo db ulandi cluster ochildi newAPI

---
+++ helper/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+module.exports = () => {
+    mongoose.connect(
+        'mongodb+srv://user_newApi:qwerty123@cluster0-fcay4.mongodb.net/test?retryWrites=true&w=majority',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+        );
+
+    mongoose.connection.on('open', () => {
+        console.log("MongoDB Is Online!");
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log("MongoDB Is Not Connected!!!",err);
+    });
+}
